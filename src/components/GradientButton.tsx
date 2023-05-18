41874df66cc0091c469b5449e5aeb843215d1dd3
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-18 15:59:40 -0300

    adiciona transições no botão

--- src/components/GradientButton.tsx
+++ src/components/GradientButton.tsx
@@ -6,7 +6,7 @@ interface GradientButtonProps {
 
 export default function GradientButton({ children }: GradientButtonProps) {
   return (
-    <button className="mt-9 w-full rounded-2xl bg-gradient py-3 text-xl font-bold leading-tight text-white sm:text-2xl">
+    <button className="mt-9 w-full rounded-2xl bg-gradient py-3 text-xl font-bold leading-tight text-white sm:text-2xl hover:brightness-110 transition-[filter] duration-200">
       {children}
     </button>
   )
