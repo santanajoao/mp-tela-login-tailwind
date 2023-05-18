44ee050a0a66f077e523697dc861e161a4b1e32f
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-18 17:08:39 -0300

    corrige padding no mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import LoginForm from '@/components/LoginForm'
 
 export default function Home() {
   return (
-    <main className="grid h-screen bg-black md:grid-cols-2">
+    <main className="grid min-h-screen bg-black md:grid-cols-2">
       {/* Left Side | SignIn Form */}
-      <div className="flex flex-col items-center justify-center px-5">
+      <div className="flex flex-col items-center justify-center p-5">
         <div className="flex w-full flex-col sm:w-auto">
           <PageTitle>
             Faça seu{' '}
             <div className="inline-block">
               login
-              <div className="ml-1 inline-block h-3 w-3 rounded-full bg-gradient" />
+              <div className="ml-1 inline-block h-2 w-2 sm:h-3 sm:w-3 rounded-full bg-gradient" />
             </div>
           </PageTitle>
 
