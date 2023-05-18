97734983799c00e0c2a8c7ee12714079ff91573c
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-18 20:04:11 -0300

    adiciona borda e blur no input

--- src/components/LabelAndInput.tsx
+++ src/components/LabelAndInput.tsx
@@ -6,13 +6,22 @@ interface LabelAndInputProps {
 
 export default function LabelAndInput({ label, id, type }: LabelAndInputProps) {
   return (
-    <label htmlFor={id} className="flex flex-col text-sm text-gray-400">
-      {label}
-      <input
-        type={type}
-        id={id}
-        className="mt-2 rounded-2xl bg-zinc-900 px-4 py-4 text-base leading-tight focus:outline-purple-700 focus:outline"
-      />
-    </label>
+    <div className="">
+      <label htmlFor={id} className="text-sm text-gray-400">
+        {label}
+      </label>
+
+      <div className="relative mt-2 flex justify-center items-center">
+        <input
+          type={type}
+          id={id}
+          className="peer rounded-2xl bg-zinc-900 px-4 py-4 text-base leading-tight text-gray-300 w-full z-10 relative outline-none"
+        />
+
+        <div className="opacity-0 bg-purple-700 h-full w-full absolute rounded-2xl bottom-0 blur-lg -z-0 peer-focus:opacity-100 transition-opacity duration-200" />
+
+        <div className="opacity-0 h-[102.2%] w-[100.35%] absolute bg-gradient rounded-2xl z-0 peer-focus:opacity-100 transition-opacity duration-200" />
+      </div>
+    </div>
   )
 }
