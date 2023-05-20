79e66171a335bfa7f69e14b517831d8621997f7d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-19 22:57:00 -0300

    estilo: remove ponto e vírgula

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import { ReactNode } from 'react';
-import './globals.css';
-import { DM_Sans as DMSans } from 'next/font/google';
+import { ReactNode } from 'react'
+import './globals.css'
+import { DM_Sans as DMSans } from 'next/font/google'
 
-const dmSans = DMSans({ subsets: ['latin'], weight: ['700', '400'] });
+const dmSans = DMSans({ subsets: ['latin'], weight: ['700', '400'] })
 
 export const metadata = {
   title: 'Most Beautyful Login',
   description: 'Uma tela de login incrivelmente bonita e responsiva usando o framework Tailwind CSS',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: ReactNode;
+  children: ReactNode
 }) {
   return (
     <html lang="pt-br">
       <body className={dmSans.className}>{children}</body>
     </html>
-  );
+  )
 }
