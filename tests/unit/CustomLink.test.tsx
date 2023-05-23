ed6adad81820049acc06186fbccec8b00c162b97
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-05-23 00:25:18 -0300

    add CustomLink href unit test

--- tests/unit/CustomLink.test.tsx
+++ tests/unit/CustomLink.test.tsx
@@ -1,10 +1,22 @@
 import CustomLink from '@/components/CustomLink'
 import { render, screen  } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-it('should display an ancor with the children text', () => {
-  const text = 'Teste'
-
-  render(<CustomLink href="/" >{text}</CustomLink>)
-
-  screen.getByRole('link', { name: text })
+describe('CustomLink', () => {
+  it('should display an ancor with the children text', () => {
+    const text = 'Teste'
+  
+    render(<CustomLink href="/" >{text}</CustomLink>)
+  
+    screen.getByRole('link', { name: text })
+  })
+  
+  it('should have the correct href', () => {
+    const href = '/teste'
+  
+    render(<CustomLink href={href} >Text</CustomLink>)
+  
+    const link = screen.getByRole('link')
+    expect(link).toHaveAttribute('href', href)
+  })
 })
